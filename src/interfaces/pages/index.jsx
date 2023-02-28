373b7306fa782043b49776a3b5e6cd8278d58299
fadeonsp <edfadeon@gmail.com>
2023-02-28 13:50:24 -0300

    componente tweet

--- src/interfaces/pages/index.jsx
+++ src/interfaces/pages/index.jsx
@@ -1,8 +1,23 @@
-
 import { useIndex } from '../../data/hooks/pages/useIndex.page'
 import styles from '../styles/pages/index.module.css'
+import TextInput from '../components/inputs/textInput/TextInput'
+import Tweet from '../components/data-display/Tweet/Tweet'
+
+ const tweet = {
+    date: 'há 3 dias',
+    text: 'Meu primeiro tweet',
+    user: {
+        name: 'Eduardo césar',
+        username: 'fadeonsp',
+        picture: 'https://github.com/fadeonsp.png',
+
+    },
+ }
 
 export default function Index() {
     useIndex()
-    return <div>Index</div>
+    return <div>
+        <TextInput />
+        <Tweet tweet={tweet} />
+    </div>
 }
