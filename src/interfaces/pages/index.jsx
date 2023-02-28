39189d6ba8986f19d4e3eac3cbec98b940b06323
fadeonsp <edfadeon@gmail.com>
2023-02-28 13:15:12 -0300

    criando estrutura do projeto

---
+++ src/interfaces/pages/index.jsx
@@ -0,0 +1,8 @@
+
+import { useIndex } from '../../data/hooks/pages/useIndex.page'
+import styles from '../styles/pages/index.module.css'
+
+export default function Index() {
+    useIndex()
+    return <div>Index</div>
+}
